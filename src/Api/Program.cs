cffb33b3a4bcd790ae54f55860f75a28dca127ce
ArwaTawfik <67197826+ArwaTawfik@users.noreply.github.com>
2026-09-18 14:13:31 +0300

    Adding The Angular Frontend Client

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -11,9 +11,19 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IJobApplicationRepository, JobApplicationRepository>();
 builder.Services.AddScoped<IJobApplicationService, JobApplicationService>(); 
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
+app.UseCors("AllowAngularDev");
 
 app.Run();
