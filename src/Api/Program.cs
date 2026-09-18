7e53abeff02345d41888b08bc3040d57945d0bf2
ArwaTawfik <67197826+ArwaTawfik@users.noreply.github.com>
2026-09-18 12:39:01 +0300

    Running Swagger And Verifying APIs are working smoothly

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -9,8 +9,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IJobApplicationRepository, JobApplicationRepository>();
-builder.Services.AddScoped<IJobApplicationService, JobApplicationService>();
+builder.Services.AddScoped<IJobApplicationService, JobApplicationService>(); 
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
 
 app.Run();
